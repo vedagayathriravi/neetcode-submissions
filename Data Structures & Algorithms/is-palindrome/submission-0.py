eb4fd075002eab0f7bdd6eeca8cf041d222ed871
vedagayathriravi <raavigayathri1@gmail.com>
2026-07-02 19:14:37 -0700

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,19 @@
+#we are using two pointers for this problem-one checks from left and one checks from right and we are considering ascii values because it needs to be in that certain range like either a-z,A-Z,0-9 .we shouldn't have any special characters.
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s) - 1
+
+        while l < r:
+            while l < r and not self.alphaNum(s[l]):
+                l += 1
+            while r > l and not self.alphaNum(s[r]):
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l, r = l + 1, r - 1
+        return True
+
+    def alphaNum(self, c):
+        return (ord('A') <= ord(c) <= ord('Z') or
+                ord('a') <= ord(c) <= ord('z') or
+                ord('0') <= ord(c) <= ord('9'))
