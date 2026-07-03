cfb8785429d981493ad49aa581009ae1d0f75569
vedagayathriravi <raavigayathri1@gmail.com>
2026-07-03 14:26:07 -0700

    Add: longest-substring-without-duplicates - submission-0

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-0.py
@@ -0,0 +1,13 @@
+#we are using sliding window for this problem.And it doesnt have to be consective alphabets.it shouldn't be repetative with max length
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        res = 0
+        for i in range(len(s)):
+            charSet = set()
+            for j in range(i, len(s)):
+                if s[j] in charSet:
+                    break
+                charSet.add(s[j])
+            res = max(res, len(charSet))
+        return res
+        
